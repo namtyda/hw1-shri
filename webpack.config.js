ba82f8e72637179ca5b25d6ccbf979d598312ecf
Andrey Pogorelov <namtydapro@gmail.com>
2020-03-06 00:02:10 +0300

    Add: start string pages, and more fix config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const miniCss = require('mini-css-extract-plugin');
-
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -13,11 +14,45 @@ module.exports = {
       use: [
         miniCss.loader,
         'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            plugins: [
+              autoprefixer({
+                overrideBrowserslist: ['last 2 version']
+              }),
+            ],
+            sourceMap: true
+          }
+        },
         'sass-loader',
-      ]
-    }]
+      ],
+    },
+    {
+      test: /\.(jpg|png|svg)$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]',
+        outputPath: 'images',
+        esModule: false
+      },
+    },
+    {
+      test: /\.html$/,
+      use: ['html-loader']
+    },
+
+    ]
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'settings.html',
+      template: './src/settings.html'
+    }),
     new miniCss({
       filename: 'style.css',
     }),
